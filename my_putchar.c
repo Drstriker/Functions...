721e3025904259c7a1f474c37ea9f06863c0863f
Thierry <thierry.yu@epitech.eu>
2016-12-16 03:18:54 +0100

    exo rendu

---
+++ my_putchar.c
@@ -0,0 +1,14 @@
+/*
+** my_putchar.c for  in /home/yu_t/Hub/GIT
+** 
+** Made by yu_t
+** Login   <yu_t@epitech.net>
+** 
+** Started on  Fri Dec 16 03:18:31 2016 yu_t
+** Last update Fri Dec 16 03:18:45 2016 yu_t
+*/
+
+void	my_putchar(char c)
+{
+  write(1, &c, 1);
+}
